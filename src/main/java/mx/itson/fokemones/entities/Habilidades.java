40c815e16ea2c45e797954d38a028be5506d1913
alang <alang@DESKTOP-JU1TSNL>
2024-12-02 17:51:26 -0700

    Se modifico el GetById de Potromones

--- src/main/java/mx/itson/fokemones/entities/Habilidades.java
+++ src/main/java/mx/itson/fokemones/entities/Habilidades.java
@@ -4,6 +4,11 @@
  */
 package mx.itson.fokemones.entities;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author zBook
@@ -55,4 +60,170 @@ public class Habilidades {
     private String habilidad;
     private Potromones protomon;
 
+}
+/**aqui se le coloca una lista de array de tipo actividad y consulta en la base de datos
+     * @param idServicio es un int el cual es el id del servicio especifico
+     * @return el int de id y orden y el string de descripcion del problema 
+     */
+    public static List<Actividad> getList(int idServicio) {
+        List<Actividad> actividades = new ArrayList<>();
+        try {
+            Connection conexion = Conexion.obtener();
+            String consulta = "SELECT id, orden, descripcion FROM actividad WHERE id_servicio = ?";
+            PreparedStatement statement = conexion.prepareStatement(consulta);
+            statement.setInt(1, idServicio);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+                Actividad a = new Actividad();
+                a.setId(rs.getInt(1));
+                a.setOrden(rs.getInt(2));
+                a.setDescripcion(rs.getString(3));
+                actividades.add(a);
+                
+            }
+        } catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        } return actividades;
+      
+    }
+    
+    private int id;
+    private int orden;
+    private String descripcion;
+    private Servicio servicio;
+    
+    /**
+     * Aqui se coloca una seccion de codigo para poder desde el programa eliminar un id en la base de datos
+     * @param id es un int el cual es el id especifico en la tabla actividad
+     * @return La eliminacion de un id especifico seleccionado
+     */
+    public static boolean delete(int id) {
+        boolean resultado = false;
+        try{
+                Connection conexion = Conexion.obtener();
+                String consulta = "DELETE FROM actividad WHERE id = ?";
+                PreparedStatement statement = conexion.prepareStatement(consulta);
+                
+                statement.setInt(1, id);
+                
+                statement.execute();
+                resultado = statement.getUpdateCount() == 1;
+                conexion.close();
+        }catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }return resultado;
+    } 
+    
+    /**
+     * Aqui esta seccion de codigo sirve para poder editar desde nuestro programa, la base de datos y poder 
+     * modificar toda la tabla de actividad
+     * @param id es un int el cual es el identificador unico de la actividad especifica 
+     * @param orden es un int el cual es el numero de orden de la actividad especifica
+     * @param descripcion es un string el cual da la descripcion del problema en el servicio especifico realizado
+     * @param idServicio es un int el cual es el id del servicio especifico 
+     * @return la edicion completada de id, orden, descripcion, idservicio
+     */
+    public static boolean edit(int id,int orden, String descripcion, int idServicio) {
+        boolean resultado = false;
+        try{
+                Connection conexion = Conexion.obtener();
+                String consulta = "UPDATE actividad SET orden = ?, descripcion = ?, id_servicio = ? WHERE id = ?";
+                PreparedStatement statement = conexion.prepareStatement(consulta);
+                statement.setInt(1, orden);
+                statement.setString(2, descripcion);
+                statement.setInt(3, idServicio);
+                statement.setInt(4,id);
+                
+                statement.execute();
+                resultado = statement.getUpdateCount() == 1;
+                conexion.close();
+        }catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }return resultado;
+    } 
+    
+    /**
+     * Esta seccion de codigo sirve para poder poder guardar nuestros cambios al realizarlos
+     * @param orden es n int el cual es la orden del servicio especifico selecconado
+     * @param descripcion es un string de la descripcion del problema especifico seleccionado
+     * @param idServicio es un int del id del servicio especifico seleccionado
+     * @return El guardado completo de nuestros cambios en la base de datos
+     */
+    public static boolean save(int orden, String descripcion, int idServicio) {
+        boolean resultado = false;
+        try{
+                Connection conexion = Conexion.obtener();
+                String consulta = "INSERT INTO actividad (orden, descripcion, id_servicio) VALUES (?, ?, ?)";
+                PreparedStatement statement = conexion.prepareStatement(consulta);
+                statement.setInt(1, orden);
+                statement.setString(2, descripcion);
+                statement.setInt(3, idServicio);
+                
+                statement.execute();
+                resultado = statement.getUpdateCount() == 1;
+                conexion.close();
+        }catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }return resultado;
+        
+        
+    } 
+    /**
+     * En esta seccion de codigo se utiliza para poder obtener el id de la persona en especifico
+     * el id de la orden en especifico y el id de la descripcion seleccionada especifica
+     * @param id es un int el cual es el id de la actividad, id de orden y id de la descripcion
+     * @return Un objeto de tipo Actividad que contiene los datos del registro correspondiente.
+     * Si no se encuentra el registro, se devuelve un objeto vacío con valores predeterminados.
+     */
+    public static Actividad getById(int id){
+            Actividad a = new Actividad ();
+        try {
+            Connection conexion = Conexion.obtener();
+            
+            String query = "SELECT id, orden, descripcion FROM actividad WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setInt(1, id);
+            
+            ResultSet rs = rs = statement.executeQuery();
+            while(rs.next()) {
+ 
+                a.setId(rs.getInt(1));
+                a.setOrden(rs.getInt(2));
+                a.setDescripcion(rs.getString(3));
+
+            }
+        } catch (Exception ex) {
+            System.err.println("Ocurrió un error " + ex.getMessage());
+        }
+        return a;
+    }
+    /**
+     * Esta seccion de codigo se usa para poder seleccionar todo lo que este dentro de "Actividad"
+     * En la base de datos
+     * @return todos los datos seleccionados de la base de datos
+     */
+    public static List<Actividad> getAll (){
+        List<Actividad> actividades = new ArrayList<>();
+        try {
+            Connection conexion = Conexion.obtener();
+            Statement statement = conexion.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT id, orden, descripcion, id_servicio FROM Actividad");
+            while(rs.next()) {
+                Actividad a = new Actividad ();
+                a.setId(rs.getInt(1));
+                a.setOrden(rs.getInt(2));               
+                a.setDescripcion(rs.getString(3));
+                
+                Servicio s = Servicio.getById(rs.getInt(4));
+                a.setServicio(s);
+                
+                actividades.add(a);
+                
+            }
+            
+        } catch(Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        } return actividades; 
+    } 
+    
 }
--- src/main/java/mx/itson/fokemones/entities/Potromones.java
+++ src/main/java/mx/itson/fokemones/entities/Potromones.java
@@ -1,9 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package mx.itson.fokemones.entities;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -117,3 +118,146 @@ public class Potromones {
   private Entrenadores entrenadores;
   private List<Habilidades> habilidad;
 }
+/**
+     * En esta seccion de codigo se utiliza para poder obtener el id del servicio en especifico
+     * la fecha de realizacion y la descripcion del problema del servicio
+     * @param id es un int el cual es el id del servicio
+     * @param fecha_realizacion es un dato tipo date el cual es la fecha que se realizo el servicio
+     * @param descripcion_problema es un dato tipo string el cual es la descripcion del problema del servicio realizado
+     * @return Un objeto de tipo Servicio que contiene los datos del registro correspondiente.
+     * Si no se encuentra el registro, se devuelve un objeto vacío con valores predeterminados.
+     */
+    public static Potromones getById(int id){
+            Potromones p = new Potromones ();
+        try {
+            Connection conexion = Conexion.obtener();
+            
+            String query = "SELECT id, nombre, descripcion, imagen, puntaje, entrenador_id, FROM Potromones WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setInt(1, id);
+            
+            ResultSet rs = rs = statement.executeQuery();
+            while(rs.next()) {
+ 
+                p.setId(rs.getInt(1));
+                p.setNombre(rs.getString(2));
+                p.setDescripcion(rs.getString(3));
+                p.setImagen(rs.getString(4));
+                p.setPuntaje(rs.getInt(5));
+
+            }
+        } catch (Exception ex) {
+            System.err.println("Ocurrió un error " + ex.getMessage());
+        }
+        return p;
+    }
+      /**
+     * Guarda un registro de Servicio en la base de datos
+     * @param fecha dato de tipo date el cual es la fecha de realizacion del servicio
+     * @param idResponsable Dato de tipo int el cual es el identificador unico del responsable que realizo el servicio
+     * @param descripcionProblema Dato de tipo String el cual es la descripcion del problema del servicio realizado
+     * @return true si se guardo exitosamente ; de lo contrario, false.
+     */
+    public static boolean save(Date fecha, int idResponsable, String descripcionProblema) {
+        boolean resultado = false;
+        try{
+                Connection conexion = Conexion.obtener();
+                String consulta = "INSERT INTO servicio (fecha_realizacion, id_responsable, descripcion_problema) VALUES (?, ?, ?)";
+                PreparedStatement statement = conexion.prepareStatement(consulta);
+                statement.setDate(1, new java.sql.Date(fecha.getTime()));
+                statement.setInt(2, idResponsable);
+                statement.setString(3, descripcionProblema);
+                statement.execute();
+                resultado = statement.getUpdateCount() == 1;
+                conexion.close();
+        }catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }return resultado;
+        
+        
+    }
+        /**
+     * Aqui esta seccion de codigo sirve para poder editar desde nuestro programa, la base de datos y poder 
+     * modificar toda la tabla de Servicio
+     * @param fecha dato de tipo date el cual es la fecha de realizacion del servicio
+     * @param idResponsable Dato de tipo int el cual es el identificador unico del responsable que realizo el servicio
+     * @param descripcionProblema Dato de tipo String el cual es la descripcion del problema del servicio realizado
+     * @return la edicion de los elementos seleccionados del servicio especificado
+     */
+    
+    public static boolean edit(Date fecha, int idResponsable, int idServicio, String descripcionProblema) {
+        boolean resultado = false;
+        try{
+                Connection conexion = Conexion.obtener();
+                String consulta = "UPDATE servicio SET fecha_realizacion = ?, descripcion_problema = ?, id_responsable = ? WHERE id = ?";
+                PreparedStatement statement = conexion.prepareStatement(consulta);
+                statement.setDate(1, new java.sql.Date(fecha.getTime()));
+                statement.setString(2, descripcionProblema);
+                statement.setInt(3, idResponsable);
+                statement.setInt(4, idServicio);
+                
+                statement.execute();
+                resultado = statement.getUpdateCount() == 1;
+                conexion.close();
+        }catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }return resultado;
+    } 
+     /**
+     * En esta seccion de codigo se elimina el id especifico seleccionado de nuestra base de datos
+     * @param idServicio es un int el cual es el identificador unico de nuestro servicio especificado
+     * @return La eliminacion del identificador unico de nuestro servicio especificado
+     */
+    public static boolean delete(int idServicio) {
+        boolean resultado = false;
+        try{
+                Connection conexion = Conexion.obtener();
+                String consulta = "DELETE FROM Servicio WHERE id = ?";
+                PreparedStatement statement = conexion.prepareStatement(consulta);
+                
+                statement.setInt(1, idServicio);
+                
+                statement.execute();
+                resultado = statement.getUpdateCount() == 1;
+                conexion.close();
+        }catch(Exception ex) {
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }return resultado;
+    } 
+    
+     /**
+     * Esta seccion de codigo se usa para poder seleccionar todo lo que este dentro de "Servicio"
+     * En la base de datos
+     * @return todos los datos seleccionados de la base de datos
+     */   
+    public static List<Servicio> getAll (){
+        List<Servicio> servicios = new ArrayList<>();
+        try {
+            Connection conexion = Conexion.obtener();
+            Statement statement = conexion.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT id, fecha_realizacion, id_responsable, descripcion_problema FROM servicio");
+            while(rs.next()) {
+                Servicio s = new Servicio ();
+                s.setId(rs.getInt(1));
+                s.setFechaRealizacion(rs.getDate(2));
+                
+                //Obtenemos un ejemplo de tipo responsable 
+                Responsable r = Responsable.getById(rs.getInt(3));
+                s.setResponsable(r);
+                
+                s.setDescripcionProblema(rs.getString(4));
+                
+                //Obtenemos una lista de tipo Actividad
+                List<Actividad> actividades = Actividad.getList(rs.getInt(1));
+                s.setActividades(actividades);
+                
+                servicios.add(s);
+                
+            }
+            
+        } catch(Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        } return servicios; 
+    } 
+}
+
